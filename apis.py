fa17b9c7868b43b0e7de3ebaa51a9dc2f3e42386
Subham Agarwal <subhamdiwan@gmail.com>
2016-03-19 05:22:00 +0530

    part 1 - Setting up out application

---
+++ apis.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+# configuration
+DEBUG = True
+SECRET_KEY = 'this-is-a-secret-key'
+
+# create our little application
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+@app.route('/')
+def index():
+    response = dict()
+    response['message'] = 'Hello world'
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run()

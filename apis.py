7affda0b5f1bc881d02f5f6892391771b3575d44
Subham Agarwal <subhamdiwan@gmail.com>
2016-03-19 05:45:09 +0530

    part 2 - Setting up the database for todo entries

--- apis.py
+++ apis.py
@@ -1,12 +1,24 @@
 from flask import Flask, jsonify
+from flask.ext.script import Manager
+import os
+from schema import db
 
 # configuration
 DEBUG = True
 SECRET_KEY = 'this-is-a-secret-key'
 
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://%s:%s@%s/%s' % (
+    os.environ['DBUSER'], os.environ['DBPASS'], os.environ['DBHOST'], os.environ['DBNAME'])
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 # create our little application
 app = Flask(__name__)
 app.config.from_object(__name__)
+db.init_app(app)
+
+manager = Manager(app)
 
 
 @app.route('/')
@@ -16,5 +28,15 @@ def index():
     return jsonify(response)
 
 
+@manager.command
+def createdb():
+    db.drop_all()
+    db.create_all()
+
+
+@manager.command
+def dropall():
+    db.drop_all()
+
 if __name__ == '__main__':
-    app.run()
+    manager.run()
